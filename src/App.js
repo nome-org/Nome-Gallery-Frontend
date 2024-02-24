13ba03394b66f1f55e982a88707de3aa506c33b6
Lazar Jankovic <GreyRabbitty@gmail.com>
2024-02-24 07:00:21 +0100

    add wallet connection

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
 // import logo from './logo.svg';
-import './App.css';
-import Body from './layout/Body';
+import "./App.css";
+import { useState } from "react";
+import Body from "./layout/Body";
+import Header from "./layout/Header";
 
-import Header from './layout/Header';
+import { UserContext } from "./Context/UserContext";
 
 function App() {
+  const [paymentAddress, setPaymentAddress] = useState("");
+  const [ordinalsAddress, setOrdinalsAddress] = useState("");
+  const [paymentPublicKey, setPaymentPublicKey] = useState("");
+  const [ordinalsPublicKey, setOrdinalsPublicKey] = useState("");
+  const [useWallet, setUseWallet] = useState(0);
+  const [paymentDerivationPath, setPaymentDerivationPath] = useState("");
+  const [ordinalDerivationPath, setOrdinalDerivationPath] = useState("");
+
   return (
-    <div className="bg-black App">
-      <Header />
-      <Body />
-    </div>
+    <UserContext.Provider
+      value={{
+        paymentAddress,
+        setPaymentAddress,
+        ordinalsAddress,
+        setOrdinalsAddress,
+        paymentPublicKey,
+        setPaymentPublicKey,
+        ordinalsPublicKey,
+        setOrdinalsPublicKey,
+        useWallet,
+        setUseWallet,
+        paymentDerivationPath,
+        setPaymentDerivationPath,
+        ordinalDerivationPath,
+        setOrdinalDerivationPath,
+      }}
+    >
+      <div className="bg-black App">
+        <Header />
+        <Body />
+      </div>
+    </UserContext.Provider>
   );
 }
 
--- src/layout/Body.jsx
+++ src/layout/Body.jsx
@@ -1,7 +1,21 @@
 import React, { useState } from 'react'
+import { getAddress, signTransaction, signMessage } from "sats-connect";
+
+import { AppConfig, UserSession, showConnect } from '@stacks/connect';
+import { StacksTestnet, StacksMainnet } from '@stacks/network';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import 'react-responsive-modal/styles.css';
 import { Modal } from 'react-responsive-modal';
+
+import { useUserContext } from "../Context/UserContext";
+
+const TEST_VERSION = false;
+
+const appConfig = new AppConfig();
+const userSession = new UserSession({ appConfig });
 
 export default function Body() {
     const [open, setOpen] = useState(false);
@@ -9,14 +23,100 @@ export default function Body() {
     const onOpenModal = () => setOpen(true);
     const onCloseModal = () => setOpen(false);
 
-    const connectWallet = async () => {
+    const [walletOpen, setWalletOpen] = useState(false);
+
+    if (userSession.isUserSignedIn()) {
+        const cardinalAddress = userSession.loadUserData().profile.btcAddress.p2wpkh.mainnet;
+        const ordinalAddress = userSession.loadUserData().profile.btcAddress.p2tr.mainnet;
+    }
+
+    const {
+        paymentAddress,
+        setPaymentAddress,
+        setOrdinalsAddress,
+        setPaymentDerivationPath,
+        setOrdinalDerivationPath,
+        setUseWallet,
+        setPaymentPublicKey,
+        setOrdinalsPublicKey,
+    } = useUserContext();
+
+    const handleOpen = () => setWalletOpen((cur) => !cur);
+    const disconnect = () => {
+        setPaymentAddress("");
+        setOrdinalsAddress("");
+        setPaymentDerivationPath("");
+        setOrdinalDerivationPath("");
+        setUseWallet(0);
+    };
+
+    const connectLeatherWallet = async () => {
+        // showConnect({
+        //     userSession,
+        //     network: StacksMainnet,
+        //     appDetails: {
+        //         name: 'App Name',
+        //         icon: window.location.origin + '/app-icon.png',
+        //     },
+        //     onFinish: () => {
+        //         console.log(
+        //             userSession.loadUserData().profile.btcAddress.p2wpkh.mainnet,
+        //             userSession.loadUserData().profile.btcAddress.p2tr.mainnet
+        //         )
+        //     },
+        //     onCancel: () => {
+        //         // handle if user closed connection prompt
+        //     },
+        // });
+
+        const userAddresses = await window.btc?.request('getAddresses');
+
+        const usersNativeSegwitAddress = userAddresses.result.addresses
+            .find(address => address.type === 'p2wpkh');
+
+        console.log('usersNativeSegwitAddress ==> ', usersNativeSegwitAddress);
+
+        const addressDetails = await fetch('https://mempool.space/api/address/' + usersNativeSegwitAddress.address);
+        console.log('addressDetails ==> ', addressDetails);
+    }
+
+    const connectUnisatWallet = async () => {
         try {
             let accounts = await window.unisat.requestAccounts();
             console.log('connect success', accounts);
-          } catch (e) {
+        } catch (e) {
             console.log('connect failed');
-          }
+        }
     }
+
+    const connectXverseWallet = async () => {
+        console.log('Xverse wallet connection ==> ')
+        const getAddressOptions = {
+            payload: {
+                purposes: ["ordinals", "payment"],
+                message: "Address for receiving Ordinals",
+                network: {
+                    type: TEST_VERSION ? "Testnet" : "Mainnet",
+                },
+            },
+            onFinish: (response) => {
+                console.log('response ==> ', response);
+                setOrdinalsAddress(response.addresses[0].address);
+                setOrdinalsPublicKey(response.addresses[0].publicKey);
+                setPaymentAddress(response.addresses[1].address);
+                setPaymentPublicKey(response.addresses[1].publicKey);
+                setUseWallet(1);
+                handleOpen();
+            },
+            onCancel: () => alert("Request canceled"),
+            onError: () => {
+                console.log("here");
+            },
+        };
+        await getAddress(getAddressOptions).catch((err) => {
+            toast.error(err.message);
+        });
+    };
 
     return (
         <>
@@ -35,13 +135,13 @@ export default function Body() {
             <Modal open={open} onClose={onCloseModal} center>
                 <div className="flex flex-col items-center w-full max-w-xs p-8 text-black bg-white rounded shadow opacity-90">
                     <h2 className="mb-8 text-3xl italic font-sans-serif">Select a wallet</h2>
-                    <button className="flex items-center w-full px-4 py-2 mb-2 font-semibold transition-all rounded hover:bg-gray-300 gap-x-4">
+                    <button className="flex items-center w-full px-4 py-2 mb-2 font-semibold transition-all rounded hover:bg-gray-300 gap-x-4" onClick={() => connectXverseWallet()}>
                         <img src="/assets/xverse-icon-0bdef1d4.png" className="h-8" /> Xverse
                     </button>
-                    <button className="flex items-center w-full px-4 py-2 mb-2 font-semibold transition-all rounded hover:bg-gray-300 gap-x-4" onClick={() => connectWallet()}>
+                    <button className="flex items-center w-full px-4 py-2 mb-2 font-semibold transition-all rounded hover:bg-gray-300 gap-x-4" onClick={() => connectUnisatWallet()}>
                         <img src="/assets/unisat-icon-df84ba56.png" className="h-8" /> Unisat
                     </button>
-                    <button className="flex items-center w-full px-4 py-2 mb-2 font-semibold transition-all rounded hover:bg-gray-300 gap-x-4">
+                    <button className="flex items-center w-full px-4 py-2 mb-2 font-semibold transition-all rounded hover:bg-gray-300 gap-x-4" onClick={() => connectLeatherWallet()}>
                         <img src="/assets/leather-icon-9a4d5194.png" className="h-8" /> Leather
                     </button>
                 </div>
